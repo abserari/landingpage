3ffdcc808ded3dc3f528e0159a76934d8c636387
abser <abserari@gmail.com>
2019-08-11 11:15:35 +0800

    new public

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import LandingIntroBanner from './components/LandingIntroBanner';
-import IceworksInfo from './components/IceworksInfo';
-import DesignLanguage from './components/DesignLanguage';
-import Materials from './components/Materials';
-import BrandList from './components/BrandList';
 
 export default function Home() {
   return (
